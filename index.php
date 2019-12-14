6f21da183702360e2a560c59a818cd94cc703fca
makeMind <33778265+makeMind@users.noreply.github.com>
2019-12-14 13:48:25 +0800

    认证是否是来自于微信的请求

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-error_log("hello".print_r($_GET, true), 3, "/www/wxpublic/log/rory.log");
-echo "hello";
-
+$signature = $_GET["signature"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];
+$echostr = $_GET["echostr"];
+$flag = $signature && $timestamp && $nonce && $echostr;
+if(!$flag){
+  exit();
+}
+$token = 'rorypublic';
+$tmpArr = array($token, $timestamp, $nonce);
+sort($tmpArr, SORT_STRING);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
+if( $tmpStr !== $signature ){
+   exit();
+}
+echo $echostr;
 ?>
